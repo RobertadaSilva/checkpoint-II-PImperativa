b6b58df51add7a71f4854b27582fab8d5dcfeaff
RobertadaSilva <robertadasilva0114.rds@gmail.com>
2021-12-10 12:57:58 -0300

    concluir

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { estudantes } = require('./estudantes.js')
+
 Aluno = require('./aluno.js').Aluno
 Estudantes = require('./estudantes.js').estudantes
 Curso = require('./curso.js').Curso
@@ -6,5 +8,5 @@ let cursoJavascript = new Curso('Introdução Javascript', 8, 5, Estudantes)
 
 cursoJavascript.addAluno(new Aluno('Rodrigo', 10, [3,5,2]))
 
-
+console.log(estudantes);
 console.log(cursoJavascript.alunosAprovados())
